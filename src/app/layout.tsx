1e82dcd5540c0a20f960818529c331888fe87785
Timur Kasimov <timakasimovktr@gmail.com>
2025-10-06 16:52:08 +0500

    dfegrth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import ClientWrapper from "./client-wrapper";
+import { SidebarProvider } from '@/context/SidebarContext';
+import { ThemeProvider } from "@/context/ThemeContext";
 
 const outfit = Inter({
   subsets: ["latin"],
@@ -20,7 +22,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
-        <ClientWrapper>{children}</ClientWrapper>
+        <ThemeProvider>
+          <SidebarProvider>
+            <ClientWrapper>{children}</ClientWrapper>
+          </SidebarProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
